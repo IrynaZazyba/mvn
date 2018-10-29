74f2cb067cecdf4b43c6a5bd30afe498ee10bdf3
IrynaZazyba <lady.ira100@gmail.com>
2018-10-29 21:47:05 +0300

    db create

---
+++ src/main/java/domain/UserRepository.java
@@ -0,0 +1,11 @@
+package domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by ladyi on 29.10.2018.
+ */
+public interface UserRepository extends CrudRepository<User,Integer>
+{
+
+}
